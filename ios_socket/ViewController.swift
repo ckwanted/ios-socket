b868ecaecbe3ee70682cea47782f0004c6f5f3c9
Mario Peñate Fariñas <ckwanted@gmail.com>
2020-03-28 11:35:59 +0100

    Initial Commit

---
+++ ios_socket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_socket
+//
+//  Created by Mario Peñate Fariñas on 28/03/2020.
+//  Copyright © 2020 Mario Peñate Fariñas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
